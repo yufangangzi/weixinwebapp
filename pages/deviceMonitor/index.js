f00980f88f0c90983c4cbe6bd5e427cfbcbcb8fd
陈刚 <769063757@qq.com>
2019-01-09 14:50:05 +0800

    ll

--- pages/deviceMonitor/index.js
+++ pages/deviceMonitor/index.js
@@ -10,10 +10,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    deviceData: {},
-    rotateSpeed: '',
-    channelnum: 4,
-    deviceNo: '2111-P230A',
     // 单元编号选择 start
     items: [
       {
@@ -39,88 +35,10 @@ Page({
     ],
     // 单元编号选择 end
   },
-  getlistNewByGroup(deviceNo) {
-    let _this = this;
-    const obj = {
-      deviceNo: '2111-P230A/B/C'
-    }
-    return util.listNewByGroup(obj)
-  },
-  getByCodes (code) {
-    let _this = this;
-    const obj = {
-      code: '2111-P230A'
-    }
-    return util.getByCode(obj)
-  },
-  initData() {
-    let _this = this;
-    Promise.all([this.getByCodes(), this.getlistNewByGroup()]).then(([res1, res2]) => {
-      if (res1.code === 0) {
-        const structure = res1.result.structure;
-        let structureNum = 4;
-        if (structure === '双支撑式离心泵') {
-          structureNum = 4;
-        } else if (structure === '悬臂式离心泵') {
-          structureNum = 2;
-        } else if (structure === '离心式压缩机') {
-          structureNum = 8;
-        }
-        _this.setData({
-          rotateSpeed: res1.result.rotateSpeed,
-          channelnum: structureNum
-        })
-        if (res2.code === 1102 && res2.result) {
-          _this.setData({
-            deviceData: handel.monitorData(res2.result, structureNum)
-          });
-        }
-      }
-    })
-
-  },
-  distributeMessage (data) {
-    if (typeof (data) === 'string') {
-      try {
-        const val = JSON.parse(data);
-        if (val.code === 1102) {
-          // 实时监控
-          this.setData({
-            deviceData: handel.monitorData(val.result, this.structureNum)
-          });
-          console.log(val);
-        }
-      } catch (e) {
-        //console.log(e);
-      }
-    }
-  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let _this= this;
-    wx.connectSocket({
-      url: util.wss + wx.getStorageSync('token')
-    })
-
-    wx.onSocketOpen(function (res) {
-      console.log('WebSocket连接已打开！')
-      wx.sendSocketMessage({
-        data: '2111-P230A/B/C'
-      })
-    })
-
-    wx.onSocketMessage(function (res) {
-      let data = res.data
-      if (data) {
-        _this.distributeMessage(data)
-      }
-    })
-
-    wx.onSocketClose(function (res) {
-      console.log('WebSocket连接已关闭！')
-    })
     this.getFiterList();  //选项
   },
 
@@ -135,7 +53,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.initData()
+    
   },
 
   /**
