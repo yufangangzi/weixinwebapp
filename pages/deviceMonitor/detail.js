d03a892598d2e70e94475a1c3fc9e41f6ff99e79
yanglinjun <yanglinjun2sinochem.com>
2019-01-15 17:10:36 +0800

    fixed4

--- pages/deviceMonitor/detail.js
+++ pages/deviceMonitor/detail.js
@@ -58,6 +58,9 @@ Page({
   
   // 打开时域波形大图
   open2SYBX() {
+    if (!this.data.btnEnabled){
+      return;
+    }
     if (this.forbiddenRepeatClicked){
       return;
     }
@@ -75,6 +78,9 @@ Page({
   },
   // 打开FFT大图
   open2FFT() {
+    if (!this.data.btnEnabled) {
+      return;
+    }
     if (this.forbiddenRepeatClicked) {
       return;
     }
