4417602a02fccf28899bec69782314412813b784
yanglinjun <yanglinjun2sinochem.com>
2019-03-14 15:41:07 +0800

    fixed5

--- pages/caselib/caselib.js
+++ pages/caselib/caselib.js
@@ -1,4 +1,3 @@
-var WxParse = require('../../dist/wxParse/wxParse.js');
 // pages/caselib/caselib.js
 import { $stopWuxRefresher } from '../../dist/wux/dist/index'
 const app = getApp();
@@ -233,6 +232,9 @@ Page({
                 if(n.tag){
                   n.tag = n.tag.replace(/,/g, " ")
                 }
+              n.title = this.convertHtmlToText(n.title);
+              n.tag = this.convertHtmlToText(n.tag);
+              n.summary = this.convertHtmlToText(n.summary);
                 return Object.assign({}, n, {
                   title: n.title,
                   tags: n.tag,
@@ -251,6 +253,9 @@ Page({
             if (n.tag) {
               n.tag = n.tag.replace(/,/g, " ")
             }
+            n.title = this.convertHtmlToText(n.title);
+            n.tag = this.convertHtmlToText(n.tag);
+            n.summary = this.convertHtmlToText(n.summary);
             return Object.assign({}, n, {
               title: n.title,
               tags: n.tag,
@@ -314,6 +319,40 @@ Page({
   pageSearch: function (event) {
     this.getPageList(this.data.inParams);
     
+  },
+  convertHtmlToText: function convertHtmlToText(inputText) {
+    var returnText = "" + inputText;
+    returnText = returnText.replace(/<\/div>/ig, '\r\n');
+    returnText = returnText.replace(/<\/li>/ig, '\r\n');
+    returnText = returnText.replace(/<li>/ig, '  *  ');
+    returnText = returnText.replace(/<\/ul>/ig, '\r\n');
+    //-- remove BR tags and replace them with line break
+    returnText = returnText.replace(/<br\s*[\/]?>/gi, "\r\n");
+
+    //-- remove P and A tags but preserve what's inside of them
+    returnText = returnText.replace(/<p.*?>/gi, "\r\n");
+    returnText = returnText.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
+
+    //-- remove all inside SCRIPT and STYLE tags
+    returnText = returnText.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
+    returnText = returnText.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
+    //-- remove all else
+    returnText = returnText.replace(/<(?:.|\s)*?>/g, "");
+
+    //-- get rid of more than 2 multiple line breaks:
+    returnText = returnText.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "\r\n\r\n");
+
+    //-- get rid of more than 2 spaces:
+    returnText = returnText.replace(/ +(?= )/g, '');
+
+    //-- get rid of html-encoded characters:
+    returnText = returnText.replace(/ /gi, " ");
+    returnText = returnText.replace(/&/gi, "&");
+    returnText = returnText.replace(/"/gi, '"');
+    returnText = returnText.replace(/</gi, '<');
+    returnText = returnText.replace(/>/gi, '>');
+
+    return returnText;
   },
   onPulling() {
     console.log('onPulling')
